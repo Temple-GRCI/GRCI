2731799f4ed500b0607bb7052d5351de09756d69
eoconnell <oconn.e@gmail.com>
2011-03-21 02:15:57 +0000

    Made some formatting changes.

--- trunk/Samples/SimplerNiviewer.cpp
+++ trunk/Samples/SimplerNiviewer.cpp
@@ -21,9 +21,10 @@ xn::DepthGenerator depth;	// Depth generator object
 
 void drawPoints(void)
 {
-	const XnDepthPixel* pDepthMap = depth.GetDepthMap();	// The XnDepthPixel pointer points to the beginning of a 1 dimensional block of memory that contains the depth information.
-															// If you have a 640x480 depth generator, then it has 307200 values.
-
+	// The XnDepthPixel pointer points to the beginning of a 1 dimensional block of memory that contains the depth information.
+	// If you have a 640x480 depth generator, then it has 307200 values.
+	const XnDepthPixel* pDepthMap = depth.GetDepthMap();	
+															
 	float count = 10.0;
 	for (int i = 0; i < (480); i++) // The depth data is organized as if you were reading from left to right, top to bottom of a 640x480 screen.
 	{						
