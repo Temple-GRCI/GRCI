7903f9d22d8a49575decbd4448bc54eda1c0cab6
prudolph <prudolph89@gmail.com>
2011-04-05 19:26:14 +0000

    START of joint recorder

---
+++ kinectJointRec/main.cpp
@@ -0,0 +1,278 @@
+/*****************************************************************************
+*                                                                            *
+*  OpenNI 1.0 Alpha                                                          *
+*  Copyright (C) 2010 PrimeSense Ltd.                                        *
+*                                                                            *
+*  This file is part of OpenNI.                                              *
+*                                                                            *
+*  OpenNI is free software: you can redistribute it and/or modify            *
+*  it under the terms of the GNU Lesser General Public License as published  *
+*  by the Free Software Foundation, either version 3 of the License, or      *
+*  (at your option) any later version.                                       *
+*                                                                            *
+*  OpenNI is distributed in the hope that it will be useful,                 *
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of            *
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the              *
+*  GNU Lesser General Public License for more details.                       *
+*                                                                            *
+*  You should have received a copy of the GNU Lesser General Public License  *
+*  along with OpenNI. If not, see <http://www.gnu.org/licenses/>.            *
+*                                                                            *
+*****************************************************************************/
+
+//status updates have been removed to improve readablity
+
+
+//---------------------------------------------------------------------------
+// Includes
+//---------------------------------------------------------------------------
+#include <XnOpenNI.h>
+#include <XnCodecIDs.h>
+#include <XnCppWrapper.h>
+#include "SceneDrawer.h"
+#include <fstream>
+//---------------------------------------------------------------------------
+// Globals
+//---------------------------------------------------------------------------
+xn::Context g_Context;// represents an OpenNi Context Object
+xn::DepthGenerator g_DepthGenerator;
+xn::UserGenerator g_UserGenerator;
+std::ofstream outpnt;
+
+XnBool g_bNeedPose = FALSE;
+XnChar g_strPose[20] = "";
+XnBool g_bDrawBackground = TRUE;
+XnBool g_bDrawPixels = TRUE;
+XnBool g_bDrawSkeleton = TRUE;
+XnBool g_bPrintID = TRUE;
+XnBool g_bPrintState = TRUE;
+
+
+#include <GL/glut.h>
+
+#define GL_WIN_SIZE_X 720
+#define GL_WIN_SIZE_Y 480
+
+XnBool g_bPause = false;
+XnBool g_bRecord = false;
+
+XnBool g_bQuit = false;
+
+//---------------------------------------------------------------------------
+// Code
+//---------------------------------------------------------------------------
+
+void CleanupExit()
+{
+	g_Context.Shutdown();
+
+	exit (1);
+}
+
+// Callback: New user was detected
+void XN_CALLBACK_TYPE User_NewUser(xn::UserGenerator& generator, XnUserID nId, void* pCookie)
+{
+	printf("New User %d\n", nId);
+	// New user found
+	if (g_bNeedPose)
+	{
+            //getPoseDetectionCap() -Gets an PoseDetectionCapability object for accessing Pose-Detection functionality(allows for a user generator to recongize when a
+            // user is ina specific pose
+            //
+		g_UserGenerator.GetPoseDetectionCap().StartPoseDetection(g_strPose, nId);//starts detection for specified pose and the user id
+                printf("**** looking for pose named: %s\n",g_strPose);
+        }
+	else
+	{
+		g_UserGenerator.GetSkeletonCap().RequestCalibration(nId, TRUE);
+	}
+}
+// Callback: An existing user was lost
+void XN_CALLBACK_TYPE User_LostUser(xn::UserGenerator& generator, XnUserID nId, void* pCookie)
+{
+	printf("Lost user %d\n", nId);
+}
+// Callback: Detected a pose
+void XN_CALLBACK_TYPE UserPose_PoseDetected(xn::PoseDetectionCapability& capability, const XnChar* strPose, XnUserID nId, void* pCookie)
+{
+	printf("Pose %s detected for user %d\n", strPose, nId);
+	g_UserGenerator.GetPoseDetectionCap().StopPoseDetection(nId);
+	g_UserGenerator.GetSkeletonCap().RequestCalibration(nId, TRUE);
+}
+// Callback: Started calibration
+void XN_CALLBACK_TYPE UserCalibration_CalibrationStart(xn::SkeletonCapability& capability, XnUserID nId, void* pCookie)
+{
+	printf("Calibration started for user %d\n", nId);
+}
+// Callback: Finished calibration
+void XN_CALLBACK_TYPE UserCalibration_CalibrationEnd(xn::SkeletonCapability& capability, XnUserID nId, XnBool bSuccess, void* pCookie)
+{
+	if (bSuccess)
+	{
+		// Calibration succeeded
+		printf("Calibration complete, start tracking user %d\n", nId);
+		g_UserGenerator.GetSkeletonCap().StartTracking(nId);
+	}
+	else
+	{
+		// Calibration failed
+		printf("Calibration failed for user %d\n", nId);
+		if (g_bNeedPose)
+		{
+			g_UserGenerator.GetPoseDetectionCap().StartPoseDetection(g_strPose, nId);
+		}
+		else
+		{
+			g_UserGenerator.GetSkeletonCap().RequestCalibration(nId, TRUE);
+		}
+	}
+}
+
+// this function is called each frame
+void glutDisplay (void)
+{
+
+	glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	// Setup the OpenGL viewpoint
+	glMatrixMode(GL_PROJECTION);
+	glPushMatrix();
+	glLoadIdentity();
+
+	xn::SceneMetaData sceneMD;//Represents a MetaData object for generators producing scene analysis maps
+	xn::DepthMetaData depthMD;
+	g_DepthGenerator.GetMetaData(depthMD);
+	glOrtho(0, depthMD.XRes(), depthMD.YRes(), 0, -1.0, 1.0);
+
+	glDisable(GL_TEXTURE_2D);
+
+	if (!g_bPause)
+	{
+		// Read next available data
+		g_Context.WaitAndUpdateAll();
+	}
+
+		// Process the data
+		g_DepthGenerator.GetMetaData(depthMD);
+		g_UserGenerator.GetUserPixels(0, sceneMD);//Get the pixels that belong to a user.
+		DrawDepthMap(depthMD, sceneMD);
+
+	glutSwapBuffers();//glutSwapBuffers swaps the buffers of the current window if double buffered.
+}
+
+void glutIdle (void)
+{
+	if (g_bQuit) {
+		CleanupExit();
+	}
+
+	// Display the frame
+	glutPostRedisplay();
+}
+
+void glutKeyboard (unsigned char key, int x, int y)
+{
+	switch (key)
+	{
+	case 27:
+		CleanupExit();
+	case 'b':
+		// Draw background?
+		g_bDrawBackground = !g_bDrawBackground;
+		break;
+	case 'x':
+		// Draw pixels at all?
+		g_bDrawPixels = !g_bDrawPixels;
+		break;
+	case 's':
+		// Draw Skeleton?
+		g_bDrawSkeleton = !g_bDrawSkeleton;
+		break;
+	case 'i':
+		// Print label?
+		g_bPrintID = !g_bPrintID;
+		break;
+	case 'l':
+		// Print ID & state as label, or only ID?
+		g_bPrintState = !g_bPrintState;
+		break;
+	case'p':
+		g_bPause = !g_bPause;
+		break;
+        case'r':
+		g_bRecord = !g_bRecord;
+                if(!g_bRecord)
+                    outpnt<<"$\n";
+                printf("Record on/off\n");
+		break;
+	}
+}
+void glInit (int * pargc, char ** argv)
+{
+	glutInit(pargc, argv);
+	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
+	glutInitWindowSize(GL_WIN_SIZE_X, GL_WIN_SIZE_Y);
+	glutCreateWindow ("Prime Sense User Tracker Viewer");
+	//glutFullScreen();
+	glutSetCursor(GLUT_CURSOR_NONE);
+
+	glutKeyboardFunc(glutKeyboard);
+	glutDisplayFunc(glutDisplay);
+	glutIdleFunc(glutIdle);
+
+	glDisable(GL_DEPTH_TEST);
+	glEnable(GL_TEXTURE_2D);
+
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glDisableClientState(GL_COLOR_ARRAY);
+}
+
+#define SAMPLE_XML_PATH "../../../../Data/SamplesConfig.xml"
+
+#define CHECK_RC(nRetVal, what)										\
+	if (nRetVal != XN_STATUS_OK)									\
+	{																\
+		printf("%s failed: %s\n", what, xnGetStatusString(nRetVal));\
+		return nRetVal;												\
+	}
+
+int main(int argc, char **argv)
+{
+    outpnt.open("kindat");
+        g_Context.Init(); //initializing the Open NI library( must be called before any other Open Ni Function( except xnInitFromXmlFile());
+        g_DepthGenerator.Create(g_Context);//creates a depth generator
+
+	g_Context.FindExistingNode(XN_NODE_TYPE_DEPTH, g_DepthGenerator);//Returns the first found existing node of the specified type(pointer to context. ,type,handle to node)
+	
+	g_Context.FindExistingNode(XN_NODE_TYPE_USER, g_UserGenerator);//
+	
+        g_UserGenerator.Create(g_Context);//creates a user generator
+		
+
+	XnCallbackHandle hUserCallbacks, hCalibrationCallbacks, hPoseCallbacks;//XnCallbackHandle-Handle to a registered callback function
+	//register***Callbacks(dectection start, detection end, null, callback handle)
+	g_UserGenerator.RegisterUserCallbacks(User_NewUser, User_LostUser, NULL, hUserCallbacks);//registers to user callbacks which are defined above
+	g_UserGenerator.GetSkeletonCap().RegisterCalibrationCallbacks(UserCalibration_CalibrationStart, UserCalibration_CalibrationEnd, NULL, hCalibrationCallbacks);
+
+	if (g_UserGenerator.GetSkeletonCap().NeedPoseForCalibration())
+	{
+		g_bNeedPose = TRUE;
+		if (!g_UserGenerator.IsCapabilitySupported(XN_CAPABILITY_POSE_DETECTION))
+		{
+			printf("Pose required, but not supported\n");
+			return 1;
+		}
+		g_UserGenerator.GetPoseDetectionCap().RegisterToPoseCallbacks(UserPose_PoseDetected, NULL, NULL, hPoseCallbacks);
+		g_UserGenerator.GetSkeletonCap().GetCalibrationPose(g_strPose);
+	}
+
+	g_UserGenerator.GetSkeletonCap().SetSkeletonProfile(XN_SKEL_PROFILE_ALL);
+
+	g_Context.StartGeneratingAll();//Make sure all generators are generating data.
+	
+
+	glInit(&argc, argv);
+	glutMainLoop();
+        
+        outpnt.close();
+}
